b37dfc2293c980fa73567870bce485b19a2ecb63
Luisa Marques dos Santos <luisamsantos.2901@gmail.com>
2026-09-04 10:08:20 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String []args){
+    System.out.println("===================");
+    System.out.println("Sistema de Projeto");
+    System.out.println("===================");
+    System.out.println("Bem-vindo(a) ao Sistema!");
+
+    System.out.println("Projeto: Portifólio Acadêmico");
+    System.out.println("Desenvolvido em Java");
+    System.out.println("versão 1.0")
+  }
+}
